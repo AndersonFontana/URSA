a1101a4b65df3445dfdebee4bd90f5cf9ed76353
AndersonFontana <andersonfontana14@hotmail.com>
2017-10-29 00:49:09 -0200

    Criada as classes de dominio da URSA

--- URSA-NetBeansProject/src/dominio/Cargo.java
+++ URSA-NetBeansProject/src/dominio/Cargo.java
@@ -5,5 +5,42 @@ package dominio;
  * @author anderson
  */
 public class Cargo {
-	
+	private int codcargo;
+	private String descricao;
+	private int tipo;
+
+	public Cargo(int codcargo, String descricao, int tipo) {
+		this.codcargo = codcargo;
+		this.descricao = descricao;
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Cargo{" + "codcargo=" + codcargo + ", descricao=" + descricao + ", tipo=" + tipo + '}';
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getCodcargo() {
+		return codcargo;
+	}
+
+	public void setCodcargo(int codcargo) {
+		this.codcargo = codcargo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
--- URSA-NetBeansProject/src/dominio/Oportunidade.java
+++ URSA-NetBeansProject/src/dominio/Oportunidade.java
@@ -1,9 +1,105 @@
 package dominio;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  *
  * @author anderson
  */
 public class Oportunidade {
+	private int codigo;
+	private int codcargo;
+	private Cargo cargo;
+	private String descricao;
+	private int acesso;
+	private Timestamp ingresso;
+	private Timestamp fechada;
+
+	public Oportunidade(int codigo, int codcargo, Cargo cargo, String descricao, int acesso, Timestamp ingresso, Timestamp fechada) {
+		this.codigo = codigo;
+		this.codcargo = codcargo;
+		this.cargo = cargo;
+		this.descricao = descricao;
+		this.acesso = acesso;
+		this.ingresso = ingresso;
+		this.fechada = fechada;
+	}
+
+	public Oportunidade(int codigo, int codcargo, String descricao, int acesso, Timestamp fechada) {
+		this.codigo = codigo;
+		this.codcargo = codcargo;
+		this.descricao = descricao;
+		this.acesso = acesso;
+		this.fechada = fechada;
+	}
+
+	@Override
+	public String toString() {
+		return "Oportunidade{" + "codigo=" + codigo + ", codcargo=" + codcargo + ", cargo=" + cargo + ", descricao=" + descricao + ", acesso=" + acesso + ", ingresso=" + ingresso + ", fechada=" + fechada + '}';
+	}
 	
+	public Oportunidade(int codigo, int codcargo, String descricao, int acesso, Date fechada) {
+		this.codigo = codigo;
+		this.codcargo = codcargo;
+		this.descricao = descricao;
+		this.acesso = acesso;
+		this.fechada = new Timestamp(fechada.getTime());
+	}
+
+	public Oportunidade() {
+		
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getCodcargo() {
+		return codcargo;
+	}
+
+	public void setCodcargo(int codcargo) {
+		this.codcargo = codcargo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getAcesso() {
+		return acesso;
+	}
+
+	public void setAcesso(int acesso) {
+		this.acesso = acesso;
+	}
+
+	public Timestamp getIngresso() {
+		return ingresso;
+	}
+
+	public void setIngresso(Timestamp ingresso) {
+		this.ingresso = ingresso;
+	}
+
+	public Timestamp getFechada() {
+		return fechada;
+	}
+
+	public void setFechada(Timestamp fechada) {
+		this.fechada = fechada;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
 }
