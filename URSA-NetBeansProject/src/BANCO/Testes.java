3f4d04d8bd173f3b18226853e39a4d70d0a19738
AndersonFontana <andersonfontana14@hotmail.com>
2017-10-29 00:53:06 -0200

    Atualizado arquivo para testes no BD

--- URSA-NetBeansProject/src/BANCO/Testes.java
+++ URSA-NetBeansProject/src/BANCO/Testes.java
@@ -1,5 +1,10 @@
 package BANCO;
 
+import dominio.Cargo;
+import dominio.Oportunidade;
+import java.sql.Timestamp;
+import java.util.List;
+
 /**
  *
  * @author anderson
@@ -11,13 +16,40 @@ public class Testes {
 	 */
 	public static void main(String[] args) {
 		DaoBanco dao = new DaoBanco();
-		String s = new String();
+		//Timestamp now = new Timestamp(System.currentTimeMillis());
+		Timestamp tstp = new Timestamp(System.currentTimeMillis()+2000000000);
+		Oportunidade op = new Oportunidade(2, 1, "Descrição 2", 7, tstp);
+		//Oportunidade opp = new Oportunidade();
+		//opp.setCodigo(1);
+		
+		// System.out.println("'"+tstp+"'");
+		// String s = new String();
+		// boolean s = false;
+		
+		List<Oportunidade> listaOportunidades = null;
+		Cargo cargo = null;
+		
 		try {
-			s = dao.teste();
+			System.out.println(dao.adicionar(op));
+			// System.out.println(dao.alterar(op));
+			// System.out.println(dao.consultar(op).toString());
+			// System.out.println(dao.excluir(op));
+			listaOportunidades = dao.listaOportunidades(1);
+			// listaOportunidades = dao.listaAbertas();
+			listaOportunidades = dao.listaAbertas(7);
+			cargo = dao.consultarCargo(1);
+			
+			System.out.println(cargo.toString());
+
+			System.out.println(listaOportunidades.size());
+			for (int i = 0; i < listaOportunidades.size(); i++) {
+				System.out.println(listaOportunidades.get(i).toString());
+			}
+			
 		} catch (Exception ex) {
-			System.out.println("Err");
+			System.out.println("Err: " + ex.getMessage());
 		}
-		System.out.println(s);
+
 	}
 	
 }
