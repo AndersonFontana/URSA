dedc11d59c88c13a72fb27e7529417915b455c3c
gabrielplicks <gabrielplicks@gmail.com>
2017-11-06 16:37:21 -0200

    Removida página Contacs
    (Veio com o template...)

--- URSApp/src/app/app.module.ts
+++ URSApp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -18,7 +17,6 @@ import { Push } from '@ionic-native/push';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
@@ -30,7 +28,6 @@ import { Push } from '@ionic-native/push';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
