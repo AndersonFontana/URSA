a11d8cda979666a71fd78fb88cd3622ac9af7fe8
gabrielplicks <gabrielplicks@gmail.com>
2017-11-06 17:25:23 -0200

    Criado pacote URSApp e método de envio de notificação

---
+++ URSA-NetBeansProject/src/URSApp/URSApp.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package URSApp;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author gpl1
+ */
+public class URSApp {
+
+    private static String SERVER_KEY = "AAAA1y5YfLg:APA91bGyqdMwpt2Z-NJoqkz22QTWpaM62_TDTgR3wF5uEFkRW8cNiGdPF9_QrGk-SKgLD85utZN3_8pmOU4AsFZTyRMuxSl4aj51ybEc1WOdUjJjeAUsFDP3BbvZBT4Nb1oHt5NO_A_F";
+    private static String DEVICE_TOKEN = "dRQmTrVPa-g:APA91bGEVvWUu0a62ZvQLQ4avdJyMhdNnyTQ7D5a-q-crqpsCRIKNpipxTphkkB4BtGVEU1AC5_3pxuD_Qe-43sq4hD7_an93cj1HrhAeB52-O1LHIeRFWnGuY1XTFO73xqw9MP8dJfl";
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String title = "Título notificação";
+        String message = "Mensagem notificação";
+        
+        try {
+            sendPushNotification(title, message);
+        } catch (Exception ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+    }
+    
+    private static void sendPushNotification(String title, String message) throws Exception {
+        String pushMessage = "{\"data\":{\"title\":\"" +
+                title +
+                "\",\"message\":\"" +
+                message +
+                "\"},\"to\":\"" +
+                DEVICE_TOKEN +
+                "\"}";
+        System.out.println(pushMessage);
+        
+        // Cria conexão para enviar FCM Message request
+        URL url = new URL("https://fcm.googleapis.com/fcm/send");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("Authorization", "key=" + SERVER_KEY);
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        OutputStream os = conn.getOutputStream();
+        os.write(pushMessage.getBytes());
+
+        System.out.println(conn.getResponseCode());
+        System.out.println(conn.getResponseMessage());
+    }
+    
+}
