ae332ff794faa435ee86f86502e1e7f6cb6c2bb7
gabrielplicks <gabrielplicks@gmail.com>
2017-11-06 16:23:54 -0200

    Config. módulos do projeto para Push notifications

--- URSApp/src/app/app.module.ts
+++ URSApp/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Importar módulo Push instalado
+import { Push } from '@ionic-native/push';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Push, // Add módulo aos provedores do app
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
