2061e83ab75b687060118f65ca4ddd9ca174a9cb
gabrielplicks <gabrielplicks@gmail.com>
2017-11-12 13:58:34 -0200

    Criada página "Detalhes" no app
    Página para mostrar informações das vagas

--- URSApp/src/app/app.module.ts
+++ URSApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { DetailsPage } from '../pages/details/details';
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -16,6 +17,7 @@ import { Push } from '@ionic-native/push';
 @NgModule({
   declarations: [
     MyApp,
+    DetailsPage,
     AboutPage,
     HomePage,
     TabsPage
@@ -27,6 +29,7 @@ import { Push } from '@ionic-native/push';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    DetailsPage,
     AboutPage,
     HomePage,
     TabsPage
--- URSApp/src/pages/home/home.ts
+++ URSApp/src/pages/home/home.ts
@@ -4,61 +4,65 @@ import { NavController } from 'ionic-angular';
 // Importa módulo Push para a página
 import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
+import { DetailsPage } from '../details/details';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private push: Push) {
+	constructor(public navCtrl: NavController, private push: Push) {
 
-  	// Checar se tem permissão para enviar push
-	this.push.hasPermission().then((res: any) => {
+	  	// Checar se tem permissão para enviar push
+		this.push.hasPermission().then((res: any) => {
 
-	    if (res.isEnabled) {
-	      alert('Tem permissão');
+		    if (res.isEnabled) {
+		      alert('Tem permissão');
 
-	    	// Inicializar push notifications
+		    	// Inicializar push notifications
 
-			const options: PushOptions = {
-				android: {
-					topics: ['ursapp']
-				},
-				ios: {
-			    	alert: 'true',
-			    	badge: true,
-			    	sound: 'false',
-			    	topics: ['ursapp']
-				},
-				windows: {},
-				browser: {
-			    	pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-				}
-			};
+				const options: PushOptions = {
+					android: {
+						topics: ['ursapp']
+					},
+					ios: {
+				    	alert: 'true',
+				    	badge: true,
+				    	sound: 'false',
+				    	topics: ['ursapp']
+					},
+					windows: {},
+					browser: {
+				    	pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+					}
+				};
 
-			const pushObject: PushObject = this.push.init(options);
+				const pushObject: PushObject = this.push.init(options);
 
-			// Este vai receber a mensagem da notificação
-			pushObject.on('notification').subscribe((notification: any) =>
-			{
-				alert('Notificação recebida: ' + notification.message);
-			});
+				// Este vai receber a mensagem da notificação
+				pushObject.on('notification').subscribe((notification: any) =>
+				{
+					alert('Notificação recebida: ' + notification.message);
 
-			// Este vai registrar no sistema de notificação e exibe o token do dispositivo
-			pushObject.on('registration').subscribe((registration: any) =>
-			{
-				document.getElementById('token').innerHTML = registration.registrationId;
-			});
+					this.navCtrl.push(DetailsPage);
+				});
 
-			// Este vai verificar se existe algum erro na mensagem
-			pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+				// Este vai registrar no sistema de notificação e exibe o token do dispositivo
+				pushObject.on('registration').subscribe((registration: any) =>
+				{
+					document.getElementById('token').innerHTML = registration.registrationId;
+				});
 
-	    } else {
-	      alert('Não tem permissão');
-	    }
+				// Este vai verificar se existe algum erro na mensagem
+				pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
 
-  	});
+		    } else {
+		      alert('Não tem permissão');
+		    }
 
-  }
+	  	});
+
+	}
 
 }
