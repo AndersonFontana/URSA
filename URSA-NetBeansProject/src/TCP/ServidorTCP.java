4f99efe5bd2e6f8482202bbb2df99d18a819287b
Daniela Kuinchtner <danielakuinchtner@gmail.com>
2017-11-18 22:36:51 -0200

    Adicionado criação de arquivo log

--- URSA-NetBeansProject/src/TCP/ServidorTCP.java
+++ URSA-NetBeansProject/src/TCP/ServidorTCP.java
@@ -8,39 +8,54 @@ package TCP;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  *
- * @author Daniela
+ * @author user
  */
 public class ServidorTCP {
 
     /**
      * @param args the command line arguments
      */
+    public static final Logger LOGGER = Logger.getLogger( ThreadServidorTCP.class.getName() );
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
         
+        int porta = 1972;
+        ServerSocket servidor = new ServerSocket(porta);
+        System.out.println("Servidor escutando porta: "+servidor.getLocalPort());
+        
         try {
-            int porta = 1972;
-            ServerSocket servidor = new ServerSocket(porta);
-            System.out.println("Servidor escutando porta: "+servidor.getLocalPort());
-            
-            while (true) {
-                Socket cliente = servidor.accept();
-                System.out.println("Servidor conectado com cliente "
-                                + cliente.getInetAddress().getHostAddress()+" na porta: "+ cliente.getPort());
-               
-                ThreadServidorTCP t1 = new ThreadServidorTCP(cliente);
-                t1.start();
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, e);
+            Handler console = new ConsoleHandler();
+            Handler file = new FileHandler("C:\\Users\\user\\Desktop\\LOG" + System.currentTimeMillis() +".txt"); //local do arquivo
+            console.setLevel(Level.WARNING);
+            file.setLevel(Level.ALL);
+            file.setFormatter(new SimpleFormatter()); // tipo do arquivo
+            LOGGER.addHandler(file);
+            LOGGER.addHandler(console);
+            LOGGER.setUseParentHandlers(false);
         }
+        catch(IOException io){
+            LOGGER.warning("O Arquivo de LOGGER não pode ser criado");
+        }
+            
+        while (true) {
+            Socket cliente = servidor.accept();
+            System.out.println("Servidor conectado com cliente "
+                            + cliente.getInetAddress().getHostAddress()+" na porta: "+ cliente.getPort());
 
-    }
+            ThreadServidorTCP t1 = new ThreadServidorTCP(cliente);
+            t1.start();
+        }
+    } 
     
 }
+    
+
